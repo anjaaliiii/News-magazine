21fb683b38a736084aa7323d96521229e594f195
anjaaliii <153795373+anjaaliii@users.noreply.github.com>
2025-12-18 13:44:18 +0530

    Handle empty news response and fix country

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -8,16 +8,28 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-        let url = `https://corsproxy.io/?https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`;
+        let url = `https://corsproxy.io/?https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`;
 
         fetch(url).then(response=>response.json()).then(data => setArticles(data.articles));
     },[])
   return (
     <div>
        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-       {Array.isArray(articles) && articles.map((news,index)=>{ 
-        return <NewsItem key={index} title ={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-    })}
+   {articles.length === 0 && (
+  <p className="text-center mt-4">No news available right now.</p>
+)}
+
+{Array.isArray(articles) && articles.map((news, index) => {
+  return (
+    <NewsItem
+      key={index}
+      title={news.title}
+      description={news.description}
+      src={news.urlToImage}
+    />
+  );
+})}
+
        
     
     </div>
