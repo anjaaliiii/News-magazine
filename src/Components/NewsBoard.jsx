177bd8c25f659132968be5e4684d528ab486135e
anjaaliii <153795373+anjaaliii@users.noreply.github.com>
2025-12-18 13:41:02 +0530

    Fix Netlify blank screen

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -8,13 +8,14 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://corsproxy.io/?https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`;
+
         fetch(url).then(response=>response.json()).then(data => setArticles(data.articles));
     },[])
   return (
     <div>
        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-       {articles.map((news,index)=>{ 
+       {Array.isArray(articles) && articles.map((news,index)=>{ 
         return <NewsItem key={index} title ={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
     })}
        
